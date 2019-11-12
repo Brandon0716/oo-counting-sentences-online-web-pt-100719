b4db03340aa509edc85e368a286d27511598693c
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-12 17:29:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,14 @@ class String
   end
 
   def question?
-
+  "I am super nervous to have my family on a plane with out me, but should i be??".end_with?("?")
   end
 
   def exclamation?
-
+   "I really dislike DCF!".end_with?("!")
   end
 
   def count_sentences
-
+   "So I must. Count the sentences. I will make three.".count_sentences
   end
 end
